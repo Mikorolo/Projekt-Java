66f04d3375b9c1f4aea8dfe0e57c2914cbbbba9b
Wiktor Chabera <wiktorch@vp.pl>
2021-05-03 22:43:21 +0200

    game logic introduction

--- src/uno/UnoGame.java
+++ src/uno/UnoGame.java
@@ -32,22 +32,117 @@ public class UnoGame
         }
     }
 
+    public void changePlayer()
+    {
+        if(gameDirection) currentPlayer++;
+        else currentPlayer--;
+
+        if(currentPlayer>playersArray.length) currentPlayer=0;
+        if(currentPlayer<0) currentPlayer=playersArray.length;
+    }
+
+    public void performSkip()
+    {
+
+        this.changePlayer();
+        playersArray[currentPlayer].searchCard(new UnoCard(UnoCard.Color.Special,UnoCard.Value.Skip));
+        this.changePlayer();
+    }
+
+    public void performDrawTwo()
+    {
+        this.changePlayer();
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+    }
+
+    public void performReverse()
+    {
+        gameDirection=!gameDirection;
+    }
+
+    public void performColorChange()
+    {
+        CurrentCard.changeColor(playersArray[currentPlayer].pickColor());
+    }
+
+    public void performWild()
+    {
+        this.performColorChange();
+
+        currentPlayer++;
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+        playersArray[currentPlayer].giveCard(Deck.drawCard());
+    }
+
+    public boolean performSpecialAction(UnoCard playedCard)
+    {
+        if(playedCard.getValue() == UnoCard.Value.drawTwo || playedCard.getValue() == UnoCard.Value.Reverse || playedCard.getValue() == UnoCard.Value.Skip)
+        {
+            if(playedCard.getColor() != this.CurrentCard.getColor())
+                return false;
+        }
+
+        if(playedCard.getValue()==UnoCard.Value.Skip)
+        {
+            CurrentCard=playedCard;
+            this.performSkip();
+            return true;
+        }
+
+        if(playedCard.getValue()==UnoCard.Value.drawTwo)
+        {
+            CurrentCard=playedCard;
+            this.performDrawTwo();
+            return true;
+        }
+
+        if(playedCard.getValue()==UnoCard.Value.Reverse)
+        {
+            CurrentCard=playedCard;
+            this.performReverse();
+            return true;
+        }
+
+        if(playedCard.getValue()==UnoCard.Value.colorChange)
+        {
+            CurrentCard=playedCard;
+            this.performColorChange();
+            return true;
+        }
+
+        if(playedCard.getValue()==UnoCard.Value.Wild)
+        {
+            CurrentCard=playedCard;
+            this.performWild();
+            return true;
+        }
+
+        return true;
+    }
+
+    public boolean performAction(UnoCard playedCard)
+    {
+        if(playedCard.getColor() == UnoCard.Color.Special) {
+            if(!performSpecialAction(playedCard))
+                {return false;}
+            else return true;
+
+        }
+
+        if(playedCard.getColor()!=this.CurrentCard.getColor() && playedCard.getValue()!=this.CurrentCard.getValue())
+            {return false;}
+
+        playersArray[currentPlayer].removeCard();//WIP
+
+        CurrentCard = playedCard;
+
+        return true;
+    }
+
     public void gameState() {
-        if(CurrentCard.getValue() == UnoCard.Value.Skip) {
-            System.out.println("Player was skipped");
-        }
-        if(CurrentCard.getValue() == UnoCard.Value.drawTwo) {
-            System.out.println("Player must draw 2 cards");
-        }
-        if(CurrentCard.getValue() == UnoCard.Value.colorChange) {
-            System.out.println("Current color was changed");
-        }
-        if(CurrentCard.getValue() == UnoCard.Value.Reverse) {
-            this.gameDirection = !this.gameDirection;
-            System.out.println("Game direction was changed");
-        }
-        if(CurrentCard.getValue() == UnoCard.Value.Wild) {
-            System.out.println("Color was changed and next player draws 4 cards");
-        }
+
     }
 }
