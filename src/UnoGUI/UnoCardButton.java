d7a7aec20e79f7476d36b19eff9c7c3614abc1be
Wiktor Chabera <wiktorch@vp.pl>
2021-06-01 21:18:24 +0200

    graphic test

---
+++ src/UnoGUI/UnoCardButton.java
@@ -0,0 +1,14 @@
+package UnoGUI;
+
+import javafx.scene.control.Button;
+import uno.UnoCard;
+
+public class UnoCardButton extends Button
+{
+//    UnoCard thisButt;
+//    public UnoCardButton(UnoCard card)
+//    {
+//        super();
+//        this.setGraphic();
+//    }
+}
--- src/UnoGUI/UnoGameController.java
+++ src/UnoGUI/UnoGameController.java
@@ -1,19 +1,41 @@
 package UnoGUI;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import uno.UnoGame;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 
 
 public class UnoGameController
 {
-    ArrayList<String> nameList;
     UnoGame game;
+    ArrayList<Image> assets = new ArrayList<>();
 
-    protected void setupGame(ArrayList<String> players)
+    @FXML
+    HBox obrazki;
+
+    protected void setupGame(ArrayList<String> players) throws Exception
     {
-        game = new UnoGame(players.size());
-        nameList = players;
+        game = new UnoGame(players.size(), players);
+        this.loadAssets();
+        for(int i=0; i<10; i++)
+            obrazki.getChildren().add(new ImageView(assets.get(i)));
     }
+
+    private void loadAssets() throws Exception
+    {
+        FileInputStream input;
+
+        for (int i=0; i<10; i++)
+        {
+            input = new FileInputStream(("E:\\Projekt-Java\\assets\\Blue_" + i + ".png"));
+            assets.add(new Image(input));
+        }
+    }
+
 
 }
